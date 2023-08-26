fb29ba988315223f96095d02572a6ad9afdd7751
OleksandrShyrokopoias <sash30.84@gmail.com>
2023-08-26 17:53:40 +0300

    step 2

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ const menu = document.querySelector('.menu__list');
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('menu__list-active');
 });
+
+const swiper = new Swiper('.swiper', {
+   
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
