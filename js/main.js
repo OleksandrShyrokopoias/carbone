040e91ff5dfaa851618b3aa4221d77f3d05b2a09
OleksandrShyrokopoias <sash30.84@gmail.com>
2023-09-17 23:06:20 +0300

    posts

--- js/main.js
+++ js/main.js
@@ -44,4 +44,22 @@ const swiperCase = new Swiper('.case-swiper', {
   },
 });
 
+// tabs
+
+const tabsTitle = document.querySelectorAll('.tab__title');
+const tabsContent = document.querySelectorAll('.tab__content');
+
+tabsTitle.forEach(item => item.addEventListener('click', event => {
+
+  const tabsTitleTarget = event.target.getAttribute('data-tab');
+  tabsTitle.forEach(element => element.classList.remove('active-tab'));
+  tabsContent.forEach(element => element.classList.add('hidden-tab-content'));
+  item.classList.add('active-tab');
+
+  document.getElementById(tabsTitleTarget).classList.remove('hidden-tab-content');
+
+}));
+
+document.querySelector('[data-tab="tab-1"]').classList.add('active-tab');
+document.querySelector('#tab-1').classList.remove('hidden-tab-content');
     
